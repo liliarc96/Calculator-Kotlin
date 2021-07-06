ce2c808236293e77e25581389e4e920c353bb974
Lília Cantalice <26139056+liliarc96@users.noreply.github.com>
2021-07-06 08:01:06 -0300

    Create calculator2.kt

---
+++ calculator2.kt
@@ -0,0 +1,24 @@
+fun main() {
+    var result:String = ""
+    println("Select the type of opperation:\n + for addition;\n - for subtraction;\n / for division;\n* for multiplication;\n")
+    println("Now imput the two numbers:\n\n")
+    var selection:String = "+"
+    println("Type a value for the first number: ")
+    var number1:Float = 2.00f
+    println("Type a value for the secont number: ")
+    var number2:Float = 4.00f
+    
+    when(selection){
+        "+" -> {result = "The result is: ${addition(number1,number2)}"}
+        "-" -> {result = "The result is: ${subtraction(number1,number2)}"}
+        "/" -> { if(!(number1.equals(0))) result = "The result is: ${division(number1,number2)}" else result = "Can't divide."}
+        "*" -> {result = "The result is: ${multiplication(number1,number2)}"}
+        else -> {println("This operation is unavailable.")}
+    }
+    println(result)
+}
+
+fun addition(n1:Float,n2:Float) = n1.plus(n2)
+fun subtraction(n1:Float,n2:Float) = n1.minus(n2)
+fun division(n1:Float,n2:Float) = n1.div(n2)
+fun multiplication(n1:Float,n2:Float) = n1.times(n2)
